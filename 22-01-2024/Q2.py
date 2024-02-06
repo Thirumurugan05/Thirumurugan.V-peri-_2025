1b54e2658817e94b88d3389a1a628e0c70f16ea9
Thirumurugan05 <157687922+Thirumurugan05@users.noreply.github.com>
2024-02-06 23:50:47 +0530

    Q2.py

---
+++ 22-01-2024/Q2.py
@@ -0,0 +1,30 @@
+Input Format
+
+The first line contains the first name, and the second line contains the last name.
+
+Constraints
+
+The length of the first and last names are each s 10.
+
+Sample Input O
+
+Submissions: 142
+
+Max Score: 10 Difficulty. Easy
+
+Rate This Challenge:
+
+More
+
+Ross
+
+Tayler
+
+Sample Output
+def print_full_name(first, last):
+    
+    print(f"Hello {first} {last}! You just delved into python.") 
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
